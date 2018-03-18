5ae9b56e46d4777c4be0743c014b45d9cda893db
TeoPolyakov <stason-2001@ya.ru>
2018-03-18 08:50:58 -0700

    Add class car

---
+++ lesson/album.js
@@ -0,0 +1,13 @@
+function Album(title, artist, year){
+    this.title=title;
+    this.artist=artist;
+    this.year=year;
+    this.play=function(){
+        //....
+    }
+}
+
+
+
+var darkSide=Album("Dark side of the Cheese","Pink Mouse", 1971);
+darkSide.play();
